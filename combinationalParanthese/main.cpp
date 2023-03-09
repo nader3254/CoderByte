a1b05989dc899df3f5b3b8d9f17518f131c3589f
nader3254 <naderhany638@gmail.com>
2023-03-10 01:30:42 +0200

    first commit

---
+++ combinationalParanthese/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+void generateParenthesis(int left, int right, string& s,vector<string>& answer)
+{
+    // terminate
+    if (left == 0 && right == 0) {
+        answer.push_back(s);
+    }
+ 
+    if (left > right || left < 0 || right < 0) {
+        // wrong
+        return;
+    }
+ 
+    s.push_back('{');
+    generateParenthesis(left - 1, right, s, answer);
+    s.pop_back();
+ 
+    s.push_back('}');
+    generateParenthesis(left, right - 1, s, answer);
+    s.pop_back();
+
+
+}
+int BracketCombinations(int num) {
+  
+    vector<string> ans;
+    string s;
+    int n = num;
+    generateParenthesis(n, n, s, ans);
+    int x=0;
+    for (auto k : ans) {
+      x++;
+    }
+ 
+  return x;
+
+}
+
+int main(void) { 
+   
+  int n = 3;
+  cout << BracketCombinations(coderbyteInternalStdinFunction(stdin));
+  return 0;
+    
+}
